d389b49979d8913efcc86a8b1a681a47ea5e4a59
Alfred Wong <alfred.cl.wong@gmail.com>
2019-11-20 14:42:49 +0000

    mtl adaptation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense, Lambda, Concatenate, Multiply, RepeatVector
+from keras.layers import Input, Dense, Lambda, Concatenate, Multiply, dot
 from keras.models import Model
 from keras import losses
 from keras import backend as K
@@ -13,6 +13,7 @@ params = {
     "gd_train_ratio": 10,
     "batch_size": 128,
     "optimizer": "adam",
+    "alpha": 2,
 }
 
 class MT_GANITE:
@@ -29,14 +30,16 @@ class MT_GANITE:
         self.gd_train_ratio = params["gd_train_ratio"]
         self.batch_size = params["batch_size"]
         self.optimizer = params["optimizer"]
+        self.alpha = params["alpha"]
 
         # Build components
         self.selector = self.build_selector()
-        self.mt_generator = self.build_multi_task_generator()
+        self.sampler = Lambda(lambda s: K.random_binomial((1, self.m), s))
+        self.mt_generators = [self.build_multi_task_generator(t) for t in range(self.k)]
         self.oh_generator = self.build_one_hot_generator()
         def mix(inputs):
-            [Yf, T, Ycfs] = inputs
-            return T * Yf + (1-T) * Ycfs
+            [yf, T, Ycfs] = inputs
+            return T * yf + (1-T) * Ycfs
         self.mixer = Lambda(mix)
         self.discriminator = self.build_discriminator()
         self.inference = self.build_inference()
@@ -45,105 +48,115 @@ class MT_GANITE:
         # Selector
         X = Input((self.m,), name="X")
         S = self.selector(X)
-        masks = Lambda(lambda s: K.random_binomial((1, self.k, self.m), s), name=f"masks")(S)
         # Generators
         Z = Input((self.k,), name="Z")
         # Multi-task Generator
-        Xs = Multiply(name="Xs")([RepeatVector(self.k)(X), masks])
-        Ycfs_mt = self.mt_generator([Xs, Z])
-        Ycfs_mt = Concatenate(name="Ycfs_mt")(Ycfs_mt)
+        masks = [self.sampler(s) for s in S]
+        Xs = [Multiply(name=f"X_{t+1}")([X, masks[t]]) for t in range(self.k)]
+        ycfs_mt = [self.mt_generators[t]([Xs[t], Z]) for t in range(self.k)]
+        Ycf_mt = Concatenate(name="Ycfs_mt")(ycfs_mt)
         # One-hot Generator
         T = Input((self.k,), name="T")
-        Yf = Input((1,), name="Yf")
-        Ycfs_oh = self.oh_generator([X, Z, T, Yf])
+        yf = Input((1,), name="yf")
+        Ycf_oh = self.oh_generator([X, Z, T, yf])
         # Discriminator
-        Ym_mt = self.mixer([Yf, T, Ycfs_mt])
+        Ym_mt = self.mixer([yf, T, Ycf_mt])
         T_mt = self.discriminator([X, Ym_mt])
-        Ym_oh = self.mixer([Yf, T, Ycfs_oh])
+        Ym_oh = self.mixer([yf, T, Ycf_oh])
         T_oh = self.discriminator([X, Ym_oh])
         # Inference
         Y_oh = self.inference(X)
 
         # Define models
-        self.mt_ganite_cf = [Model([X, Z], T_mt, name=f"MT_GANITE_CF_{t+1}") for t in range(self.k)]
-        self.oh_ganite_cf = Model([X, Z, T_, Yf], T_oh, name="OH_GANITE_CF")
-        self.mt_ganite = Model([X, Z], Ycfs_mt, name="MT_GANITE")
-        self.oh_ganite = Model([X, Z, T, Yf], Y_oh, name="OH_GANITE")
+        self.mt_ganite_cfs = [Model([X, Z, T, yf], T_mt, name=f"MT_GANITE_CF_{t+1}") for t in range(self.k)]
+        self.oh_ganite_cf = Model([X, Z, T, yf], T_oh, name="OH_GANITE_CF")
+        #self.mt_ganite = Model([X, Z], Ycfs_mt, name="MT_GANITE")
+        #self.oh_ganite = Model([X, Z, T, yf], Y_oh, name="OH_GANITE")
 
         # Define losses
-        def d_loss_fn(y_true, y_pred):
-            return losses.categorical_crossentropy(y_true, y_pred)
-        def mtg_loss_fn():
-            pass
-        def ohg_loss_fn():
+        def d_loss_fn():
+            return losses.categorical_crossentropy
+        def mtg_loss_fn(yf, ycf):
+            supervised_loss = K.square(yf - ycf)
+            def loss(y_true, y_pred):
+                return supervised_loss - self.alpha * d_loss_fn()(y_true, y_pred)
+            return loss
+        def ohg_loss_fn(yf, T, Ycf):
+            supervised_loss = K.square(yf - dot([T, Ycf], axes=-1))
+            def loss(y_true, y_pred):
+                return supervised_loss - self.alpha * d_loss_fn()(y_true, y_pred)
+            return loss
+        def s_loss_fn():
             pass
 
         # Compile neural networks
-        self.discriminator.compile(loss=d_loss_fn, optimizer=self.optimizer)
+        self.discriminator.compile(loss=d_loss_fn(), optimizer=self.optimizer)
         self.discriminator.trainable = False
-        for Ycf_mt_model in self.mt_ganite:
-            Ycf_mt_model.compile(loss=mtg_loss_fn, optimizer=self.optimizer)
+        for mt_generator in self.mt_generators:
+            mt_generator.trainable = False
+        for t in range(self.k):
+            self.mt_generators[t].trainable = True
+            self.mt_ganite_cfs[t].compile(loss=mtg_loss_fn(yf, ycfs_mt[t]), optimizer=self.optimizer)
+            self.mt_generators[t].trainable = False
+        self.oh_generator.compile(loss=ohg_loss_fn(yf, T, Ycf_oh), optimizer=self.optimizer)
+        #self.selector.compile(...)
 
-    def train(self, X_train, T_train, Yf_train):
+    def train(self, X_train, T_train, yf_train):
         def get_batch(batch_size=self.batch_size):
             N_train = X_train.shape[0]
             idx = np.random.randint(N_train, size=batch_size)
             X = X_train[idx]
             Z = self.sample_Z(batch_size)
             T = T_train[idx]
-            Yf = Yf_train[idx]
-            return X, Z, T, Yf
+            yf = yf_train[idx]
+            return X, Z, T, yf
 
         for it in tqdm(range(self.n_iters)):
             # Train discriminator on both mt and oh generators
             for i in range(self.gd_train_ratio):
                 # mt generator
-                X, Z, T, Yf = get_batch()
-                Ycfs_mt = self.mt_ganite([X, Z])
-                Ycfs_mt = Concatenate(name="Ycfs_mt")(Ycfs_mt)
-                Ym_mt = T * Yf + (1-T) * Ycfs_mt
-                d_loss = self.discriminator.train_on_batch([X, Ym_mt], T)
+                X, Z, T, yf = get_batch()
+                Ycf_mt = [self.mt_generators[t].predict([X, Z])]
+                # TODO concat and sub in yfs
+                #Ycf_mt = Concatenate(axis=1)([Lambda(lambda x: K.expand_dims(x, axis=1))(x) for x in Ycf_mt])
+                #d_loss = self.discriminator.train_on_batch([X, Ym_mt], T)
 
                 # oh generator
-                X, Z, T, Yf = get_batch()
-                Ycfs_oh = self.ganite([X, Z, T, Yf])
-                Ym_oh = T * Yf + (1-T) * Ycfs_oh
+                X, Z, T, yf = get_batch()
+                Ycf_oh = self.ganite([X, Z, T, yf])
+                Ym_oh = T * yf + (1-T) * Ycf_oh
                 d_loss = self.discriminator.train_on_batch([X, Ym_oh], T)
 
     def predict(self, X):
         return self.mt_ganite.predict([X, Z])
 
-    def build_selector(self):
+    def build_selector(self): # todo init uniform: zeros?
         X = Input((self.m,), name="X")
         hidden = X
         for h_layer in range(self.h_layers):
             hidden = Dense(self.h_dim, activation=self.activation, name=f"h_{h_layer+1}")(hidden)
         S = [Dense(self.m, activation="sigmoid", name=f"S_{t+1}")(hidden) for t in range(self.k)]
-        S = Concatenate(axis=1)([Lambda(lambda x: K.expand_dims(x, axis=1))(s) for s in S])
         return Model(X, S, name="Selector")
 
-    def build_multi_task_generator(self):
-        Xs = Input((self.k, self.m), name="Xs")
+    def build_multi_task_generator(self, t):
+        xs = Input((self.m,), name="xs")
         Z = Input((self.k,), name="Z")
-        Ycfs = []
-        for t in range(self.k):
-            hidden = Concatenate()([Lambda(lambda x: x[:,t,:])(Xs), Z])
-            for h_layer in range(self.h_layers):
-                hidden = Dense(self.h_dim, activation=self.activation, name=f"h_{t+1}_{h_layer+1}")(hidden)
-            Ycfs.append(Dense(1, name=f"Ycf_{t+1}")(hidden))
-        Ycf = Concatenate(name="Ycf")(Ycfs)
-        return Model([Xs, Z], Ycfs, name="MT_Generator")
+        hidden = Concatenate()([xs, Z])
+        for h_layer in range(self.h_layers):
+            hidden = Dense(self.h_dim, activation=self.activation, name=f"h_{h_layer+1}")(hidden)
+        ycf = Dense(1, name=f"ycf")(hidden)
+        return Model([xs, Z], ycf, name=f"MT_Generator_{t+1}")
 
     def build_one_hot_generator(self):
         X = Input((self.m,), name="X")
         Z = Input((self.k,), name="Z")
         T = Input((self.k,), name="T")
-        Yf = Input((1,), name="Yf")
-        hidden = Concatenate()([X, Z, T, Yf])
+        yf = Input((1,), name="yf")
+        hidden = Concatenate()([X, Z, T, yf])
         for h_layer in range(self.h_layers):
             hidden = Dense(self.h_dim, activation=self.activation, name=f"h_{h_layer+1}")(hidden)
         Ycf = Dense(self.k, name="Ycf")(hidden)
-        return Model([X, Z, T, Yf], Ycf, name="OH_Generator")
+        return Model([X, Z, T, yf], Ycf, name="OH_Generator")
 
     def build_discriminator(self):
         X = Input((self.m,), name="X")
