f2ebd3695317d3c486ea7318a45f6c4e2b694b4a
Alfred Wong <alfred.cl.wong@gmail.com>
2019-11-21 01:01:48 +0000

    mt_gen, oh_gen and discriminator training implemented

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from keras.layers import Input, Dense, Lambda, Concatenate, Multiply, dot
 from keras.models import Model
 from keras import losses
+from keras.utils import to_categorical
 from keras import backend as K
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
 params = {
     "h_layers": 2,
     "h_dim": 8,
     "activation": "relu",
-    "n_iters": 1000,
-    "gd_train_ratio": 10,
+    "n_iters": 2000,
+    "gd_train_ratio": 5,
     "batch_size": 128,
     "optimizer": "adam",
     "alpha": 2,
@@ -34,7 +36,7 @@ class MT_GANITE:
 
         # Build components
         self.selector = self.build_selector()
-        self.sampler = Lambda(lambda s: K.random_binomial((1, self.m), s))
+        self.sampler = Lambda(lambda s: K.random_binomial((self.batch_size, self.m), s), name="sampler")
         self.mt_generators = [self.build_multi_task_generator(t) for t in range(self.k)]
         self.oh_generator = self.build_one_hot_generator()
         def mix(inputs):
@@ -51,9 +53,10 @@ class MT_GANITE:
         # Generators
         Z = Input((self.k,), name="Z")
         # Multi-task Generator
-        masks = [self.sampler(s) for s in S]
+        masks = [self.sampler(s) for s in S] 
         Xs = [Multiply(name=f"X_{t+1}")([X, masks[t]]) for t in range(self.k)]
-        ycfs_mt = [self.mt_generators[t]([Xs[t], Z]) for t in range(self.k)]
+        #ycfs_mt = [self.mt_generators[t]([Xs[t], Z]) for t in range(self.k)]
+        ycfs_mt = [self.mt_generators[t]([X, Z]) for t in range(self.k)] # tmp: skip selector
         Ycf_mt = Concatenate(name="Ycfs_mt")(ycfs_mt)
         # One-hot Generator
         T = Input((self.k,), name="T")
@@ -70,7 +73,7 @@ class MT_GANITE:
         # Define models
         self.mt_ganite_cfs = [Model([X, Z, T, yf], T_mt, name=f"MT_GANITE_CF_{t+1}") for t in range(self.k)]
         self.oh_ganite_cf = Model([X, Z, T, yf], T_oh, name="OH_GANITE_CF")
-        #self.mt_ganite = Model([X, Z], Ycfs_mt, name="MT_GANITE")
+        self.mt_ganite = Model([X, Z], Ycf_mt, name="MT_GANITE")
         #self.oh_ganite = Model([X, Z, T, yf], Y_oh, name="OH_GANITE")
 
         # Define losses
@@ -90,6 +93,8 @@ class MT_GANITE:
             pass
 
         # Compile neural networks
+        #self.selector.compile(...)
+        self.selector.trainable = False
         self.discriminator.compile(loss=d_loss_fn(), optimizer=self.optimizer)
         self.discriminator.trainable = False
         for mt_generator in self.mt_generators:
@@ -98,8 +103,7 @@ class MT_GANITE:
             self.mt_generators[t].trainable = True
             self.mt_ganite_cfs[t].compile(loss=mtg_loss_fn(yf, ycfs_mt[t]), optimizer=self.optimizer)
             self.mt_generators[t].trainable = False
-        self.oh_generator.compile(loss=ohg_loss_fn(yf, T, Ycf_oh), optimizer=self.optimizer)
-        #self.selector.compile(...)
+        self.oh_ganite_cf.compile(loss=ohg_loss_fn(yf, T, Ycf_oh), optimizer=self.optimizer)
 
     def train(self, X_train, T_train, yf_train):
         def get_batch(batch_size=self.batch_size):
@@ -111,24 +115,46 @@ class MT_GANITE:
             yf = yf_train[idx]
             return X, Z, T, yf
 
+        ts = np.argmax(T_train, axis=1)
+        idxs = [np.where(ts==t)[0] for t in range(self.k)]
         for it in tqdm(range(self.n_iters)):
             # Train discriminator on both mt and oh generators
             for i in range(self.gd_train_ratio):
                 # mt generator
                 X, Z, T, yf = get_batch()
-                Ycf_mt = [self.mt_generators[t].predict([X, Z])]
-                # TODO concat and sub in yfs
-                #Ycf_mt = Concatenate(axis=1)([Lambda(lambda x: K.expand_dims(x, axis=1))(x) for x in Ycf_mt])
-                #d_loss = self.discriminator.train_on_batch([X, Ym_mt], T)
+                Ycf_mt = np.concatenate([self.mt_generators[t].predict([X, Z]) for t in range(self.k)], axis=1)
+                Ym_mt = (T.T * yf).T + (1-T) * Ycf_mt
+                d_loss_mtg = self.discriminator.train_on_batch([X, Ym_mt], T)
 
                 # oh generator
                 X, Z, T, yf = get_batch()
-                Ycf_oh = self.ganite([X, Z, T, yf])
-                Ym_oh = T * yf + (1-T) * Ycf_oh
-                d_loss = self.discriminator.train_on_batch([X, Ym_oh], T)
+                Ycf_oh = self.oh_generator.predict([X, Z, T, yf])
+                Ym_oh = (T.T * yf).T + (1-T) * Ycf_oh
+                d_loss_ohg = self.discriminator.train_on_batch([X, Ym_oh], T)
+
+            ## Train generators
+            # mt generator
+            for t in range(self.k):
+                idx = np.random.choice(idxs[t], size=self.batch_size, replace=False)
+                X = X_train[idx]
+                Z = self.sample_Z(self.batch_size)
+                T = T_train[idx]
+                yf = yf_train[idx]
+                mtg_loss = self.mt_ganite_cfs[t].train_on_batch([X, Z, T, yf], T)
+
+            # oh generator
+            X, Z, T, yf = get_batch()
+            ohg_loss = self.oh_ganite_cf.train_on_batch([X, Z, T, yf], T)
+
+            if it % (self.n_iters//10) == 0:
+                print(f"Iter: {it:4d}/{self.n_iters:4d}")
+                print(f"d_loss_mtg: {d_loss_mtg:2.4f} mtg_loss: {mtg_loss:2.4f}")
+                print(f"d_loss_ohg: {d_loss_ohg:2.4f} ohg_loss: {ohg_loss:2.4f}")
 
     def predict(self, X):
-        return self.mt_ganite.predict([X, Z])
+        # TODO Sample over multiple Z for each x
+        Z = self.sample_Z(X.shape[0])
+        return self.mt_ganite.predict([X, Z], batch_size=self.batch_size)
 
     def build_selector(self): # todo init uniform: zeros?
         X = Input((self.m,), name="X")
@@ -179,6 +205,38 @@ class MT_GANITE:
         return np.random.uniform(-1, 1, size=[N, self.k])
 
 if __name__ == "__main__":
+    np.set_printoptions(formatter={"float_kind": lambda x: f"{x:.4f}"})
+
+    # Load data
+    data = pd.read_csv("Twin_Data.csv").values
+    N = data.shape[0]
     m = 30
     k = 2
+
+    # Preprocess
+    X = data[:, :m]
+    X = X - X.min(axis=0)
+    X = X / X.max(axis=0)
+    Y = data[:, m:]
+    Y[Y>365] = 365
+    Y = 1 - Y/365.0
+    t = np.random.randint(k, size=N)
+    T = to_categorical(t, num_classes=k, dtype="int32")
+    yf = np.choose(t, Y.T)
+
+    # Train/test split
+    N_train = N
+    while N_train > 0.8 * N:
+        N_train -= params["batch_size"]
+    X_train = X[:N_train]
+    yf_train = yf[:N_train]
+    T_train = T[:N_train]
+    X_test = X[N_train:]
+    yf_test = yf[N_train:]
+    T_test = T[N_train:]
+
+    # MT_GANITE
     mt_ganite = MT_GANITE(m, k)
+    mt_ganite.train(X_train, T_train, yf_train)
+    print(mt_ganite.predict(X_test))
+    print(Y[N_train:])
