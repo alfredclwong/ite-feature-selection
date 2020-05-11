54ede7c1e02bcacfe90f45fa725c563734e2d2b0
Alfred Wong <alfred.cl.wong@gmail.com>
2020-05-11 17:52:47 +0100

    added auc for testing purposes

--- utils/metrics.py
+++ utils/metrics.py
@@ -7,6 +7,7 @@ def shape_check(y_true, y_pred):
     assert len(y_true.shape) == 2
     assert y_true.shape[1] == 2
 
+
 def PEHE(y_true, y_pred, norm=False):
     shape_check(y_true, y_pred)
     true_effect = y_true[:, 1] - y_true[:, 0]
@@ -20,10 +21,12 @@ def PEHE(y_true, y_pred, norm=False):
         rmse = np.sqrt(np.mean(sq_error))
         return rmse
 
+
 def r2(y_true, y_pred):
     rss = np.sum(np.square(y_true - y_pred))
     tss = np.sum(np.square(y_true - np.mean(y_true)))
     return 1 - rss/tss
+
 
 def tpr_fdr(y_true, y_pred):
     tn, fp, fn, tp = confusion_matrix(y_true.flatten(), y_pred.flatten()).ravel()
@@ -31,12 +34,19 @@ def tpr_fdr(y_true, y_pred):
     fdr = fp / (tp + fp)
     return tpr, fdr
 
+
 def roc(y_true, y_pred):
     thresholds = np.arange(0, 1+1e-5, 1e-2)
-    roc = np.zeros((thresholds.size, 2))
+    r = np.zeros((thresholds.size, 2))
     for i in range(thresholds.size):
-        tn, fp, fn, tp = confusion_matrix(y_true.flatten(), y_pred.flatten()>=thresholds[i]).ravel()
+        tn, fp, fn, tp = confusion_matrix(y_true.flatten(), y_pred.flatten() > thresholds[i]).ravel()
         tpr = tp / (tp + fn)
         fpr = fp / (tn + fp)
-        roc[i] = [fpr, tpr]
-    return np.unique(roc, axis=0)
+        r[i] = [fpr, tpr]
+    return np.unique(r, axis=0)
+
+
+def auc(y_true, y_pred):
+    r = roc(y_true, y_pred)
+    a = np.trapz(r[:, 1], x=r[:, 0])
+    return a
