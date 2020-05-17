37f0f4959c5068fb6275387ca44da43b3dd2cab2
Alfred Wong <alfred.cl.wong@gmail.com>
2020-05-17 01:38:44 +0100

    added oracle results and vectorised some code

--- 1-propensity.py
+++ 1-propensity.py
@@ -8,8 +8,10 @@ from tqdm import tqdm
 import pandas as pd
 
 
+def p(X): return expit(X[:, 0] * X[:, 1] + X[:, 2])
+
+
 def predict(X, T, Y, Y_true=None):
-    T = T.astype(int)
     n_treatments = len(set(T))
     n, n_features = X.shape
 
@@ -17,28 +19,25 @@ def predict(X, T, Y, Y_true=None):
     invase = Invase(n_features, n_treatments, hyperparams=hyperparams)
     invase.train(X_train, T_train, 500, X_test, T_test, verbose=False, save_history=False)
 
-    # Store two sets of propensity scores: one from full INVASE, the other from the baseline model
-    propensity_scores = np.zeros((2, n, 2))
-    propensity_scores[0] = invase.predict(X, threshold=None)
-    propensity_scores[1] = invase.predict(X, threshold=None, use_baseline=True)
+    # Store three sets of propensity scores: oracle, INVASE, baseline
+    n_models = 3
+    propensity_scores = np.zeros((n_models, n, n_treatments))
+    propensity_scores[0, :, 1] = p(X)
+    propensity_scores[0, :, 0] = 1 - propensity_scores[0, :, 1]
+    propensity_scores[1] = invase.predict(X, threshold=None)
+    propensity_scores[2] = invase.predict(X, threshold=None, use_baseline=True)
 
-    # Calculate two sets of corresponding weights
-    weights = np.zeros((2, n))
-    pT = [np.mean(T == 0)]
-    pT.append(1 - pT[0])
-    for i in range(n):
-        weights[0, i] = pT[T[i]] / propensity_scores[0, i, T[i]]
-        weights[1, i] = pT[T[i]] / propensity_scores[1, i, T[i]]
+    # Calculate three sets of corresponding weights
+    pT = np.array([np.mean(T == t) for t in range(n_treatments)])
+    weights = pT[T] / propensity_scores[:, np.arange(n), T]  # (3, n)
 
     # Output max weights from each model for diagnostics
     max_weights = np.max(weights, axis=1)
 
     # Return IPW ATE estimates for each model
-    Yw = Y * weights[0]
-    ate_invase = np.mean(Yw[T == 1]) - np.mean(Yw[T == 0])
-    Yw = Y * weights[1]
-    ate_baseline = np.mean(Yw[T == 1]) - np.mean(Yw[T == 0])
-    return ate_invase, ate_baseline, max_weights
+    Y_weighted = Y * weights
+    ates_ipw = np.mean(Y_weighted[:, T == 1], axis=1) - np.mean(Y_weighted[:, T == 0], axis=1)
+    return ates_ipw, max_weights
 
 
 default_env()
@@ -54,10 +53,10 @@ hyperparams = {
 
 # Load saved progress file and perform more trials if necessary
 results_path = 'results/1-propensity.csv'
-headers = 'true naive invase base max(w_i) max(w_b)'
+headers = 'true naive oracle invase base max(W) max(Wi) max(Wb)'
 n_trials = 1000
 progress = 0
-results = np.zeros((n_trials, 6))
+results = np.zeros((n_trials, 8))
 try:
     _results = pd.read_csv(results_path, index_col=0).values
     assert(_results.shape[1] == results.shape[1])
@@ -72,7 +71,7 @@ except (IOError, IndexError):
         X = np.random.standard_normal(size=(1000, 11))
         n, n_features = X.shape
 
-        T = np.random.binomial(1, p=expit(X[:, 0] * X[:, 1] + X[:, 2]))
+        T = np.random.binomial(1, p=p(X))
         n_treatments = int(np.max(T)) + 1
 
         beta = np.random.normal(np.ones(n_features), 1)
@@ -84,8 +83,8 @@ except (IOError, IndexError):
         # Perform IPW, report all estimates and biases, save progress
         ate_true = np.mean(Y[:, 1] - Y[:, 0])
         ate_naive = np.mean(Yf[T == 1]) - np.mean(Yf[T == 0])
-        ate_invase, ate_baseline, max_weights = predict(X, T, Yf, Y_true=Y)
-        results[i] = [ate_true, ate_naive, ate_invase, ate_baseline] + list(max_weights)
+        ates_ipw, max_weights = predict(X, T, Yf, Y_true=Y)
+        results[i] = [ate_true, ate_naive] + list(ates_ipw) + list(max_weights)
         ebias = 2 * 0.373 * beta[2]
         bias = ate_naive - ate_true
         print('\n'.join(map(lambda t: ''.join(t), zip(
@@ -100,12 +99,16 @@ print('\t'.join([''] + headers.split()))
 print('\t'.join(['mean'] + list(map(lambda x: f'{x:.3f}', np.mean(results, axis=0)))))
 print('\t'.join(['std'] + list(map(lambda x: f'{x:.3f}', np.std(results, axis=0)))))
 
-plt.figure()
-x_grid = np.linspace(-2, 5, 100)
-for i in range(results.shape[1]):
+plt.figure(figsize=(4, 2.5))
+x_grid = np.linspace(-1, 3, 100)
+for i in range(5):
     pdf = est_pdf(results[:, i], x_grid)
     plt.plot(x_grid, pdf)
 plt.legend(headers.split())
+plt.xlabel('ATE estimate')
+plt.ylabel('Probability')
+plt.xticks(np.arange(4))
 plt.xlim([np.min(x_grid), np.max(x_grid)])
 plt.ylim([0, 4])
+plt.savefig('../iib-diss/1-propensity.pdf', bbox_inches='tight')
 plt.show()
