f27ec8259474d2ac80a367c1ef932ed205ed5c45
Alfred Wong <alfred.cl.wong@gmail.com>
2020-05-27 00:08:17 +0100

    save witness function for nice appendix note

--- misc/ipm.py
+++ misc/ipm.py
@@ -7,7 +7,7 @@ k = lambda x, y: rbf_kernel(x, y, .5)
 g = lambda x: np.exp(-xs**2/2) / np.sqrt(2*np.pi)
 l = lambda x: np.exp(-abs(xs))/2
 
-n = int(2e3)
+n = int(2e4)
 a = np.random.normal(0, 1, size=n).reshape(-1, 1)
 b = np.random.laplace(0, 1, size=n).reshape(-1, 1)
 xs = np.linspace(-6, 6, n).reshape(-1, 1)
@@ -17,7 +17,8 @@ h = k(a, a) + k(b, b) - k(a, b) - k(b, a)
 print(np.mean(h[~np.eye(n, dtype=bool)]))  # emp mmd
 print(np.mean(w))
 
-plt.plot(xs, g)
-plt.plot(xs, l)
+plt.plot(xs, g(xs))
+plt.plot(xs, l(xs))
 plt.plot(xs, w)
+plt.savefig('../iib-diss/slides/witness.png', bbox_inches='tight')
 plt.show()
